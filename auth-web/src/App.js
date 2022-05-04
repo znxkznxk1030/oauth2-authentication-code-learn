6b2633102f8d2b84db9e89232f1cda335edd4363
ysKim <znxkznxk1030@gmail.com>
2022-05-04 21:38:36 +0200

    [ADD] form

--- auth-web/src/App.js
+++ auth-web/src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = ({ target: { value } }) => {
+    setUsername(value);
+  };
+
+  const handlePassword = ({ target: { value } }) => {
+    setPassword(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
+
   return (
     <div className="App">
-      <form>
-        
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input type="text" value={username} onChange={handleUsername} />
+        </label>
+        <label>
+          Password
+          <input type="password" value={password} onChange={handlePassword} />
+        </label>
+        <button type="submit">
+          로그인
+        </button>
       </form>
     </div>
   );
